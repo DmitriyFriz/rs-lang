38b76db02dbdebe7217ec5907e0552814eab6a44
EvgeniyGuzenkov <jack199479@gmail.com>
2020-06-22 00:09:03 +0300

    feat: add Statistics domain model

---
+++ src/domain-models/Statistics/Statistics.js
@@ -0,0 +1,22 @@
+import BaseDomainModel from '../BaseDomainModel/BaseDomainModel';
+import endPoints from '../../services/endPoints/endPoints.main';
+
+const { get, update } = endPoints.statistics;
+
+class Statistics extends BaseDomainModel {
+  async getStatistics() {
+    const res = await this.getDataOfAuthorizedUser(
+      get, this.userId, this.token,
+    );
+    return res;
+  }
+
+  async updateStatistics(data) {
+    const res = await this.getDataOfAuthorizedUser(
+      update, this.userId, this.token, data,
+    );
+    return res;
+  }
+}
+
+export default Statistics;
