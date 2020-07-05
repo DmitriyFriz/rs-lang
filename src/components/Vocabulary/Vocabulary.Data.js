620d40b8de2b11a57c98ab1e0f59fb8c06debd98
marinamarine <marinakarpovskaya@gmail.com>
2020-07-05 04:33:34 +0300

    feat: implement prepareData and addListeners Vocabulary methods

--- src/components/Vocabulary/Vocabulary.Data.js
+++ src/components/Vocabulary/Vocabulary.Data.js
@@ -1,81 +1,107 @@
 import { VOCABULARY_ROUTERS, MAIN_ROUTES } from '../../router/Router.Constants';
 
-const vocabularyPageLayout = {
+const pageLayout = {
+  container: {
+    className: 'vocabulary',
+  },
   info: {
-    tag: 'div',
     content1: 'AMOUNT OF WORDS:',
     content2: 'ADDED TODAY:',
     className: 'vocabulary__info',
   },
   nav: {
-    tag: 'div',
     className: 'vocabulary__nav',
   },
   list: {
-    tag: 'div',
     className: 'vocabulary__list',
   },
   item: {
-    tag: 'div',
     className: 'vocabulary__item',
+  },
+  word: {
+    className: 'vocabulary__word',
+  },
+  transcription: {
+    className: 'vocabulary__transcription',
+  },
+  audio: {
+    content: 'play',
+    className: 'vocabulary__audio',
+  },
+  translation: {
+    className: 'vocabulary__translation',
+  },
+  image: {
+    className: 'vocabulary__image',
+  },
+  meaning: {
+    className: 'vocabulary__meaning',
+  },
+  example: {
+    className: 'vocabulary__example',
+  },
+  remove: {
+    content: 'x',
+    className: 'vocabulary__remove',
+  },
+  noWords: 'Nothing to show',
+  pagination: {
+    className: 'vocabulary__pagination',
+  },
+  prev: {
+    content: '<<',
+    className: 'vocabulary__prev',
+  },
+  next: {
+    content: '>>',
+    className: 'vocabulary__next',
   },
 };
 
 const navTabsLayout = [
   {
-    tag: 'div',
     content: 'Learning words',
     className: 'vocabulary__tab',
     destination: MAIN_ROUTES.VOCABULARY,
   },
   {
-    tag: 'div',
     content: 'Difficult words',
     className: 'vocabulary__tab',
     destination: VOCABULARY_ROUTERS.VOCABULARY_DIFFICULT,
   },
   {
-    tag: 'div',
     content: 'Deleted words',
     className: 'vocabulary__tab',
     destination: VOCABULARY_ROUTERS.VOCABULARY_DELETED,
   },
 ];
 
-const tempWords = [
-  {
-    audio: 'files/02_0621.mp3',
-    audioExample: 'files/02_0621_example.mp3',
-    audioMeaning: 'files/02_0621_meaning.mp3',
-    group: 1,
-    id: '5e9f5ee35eb9e72bc21af70c',
-    image: 'files/02_0621.jpg',
-    page: 1,
-    textExample: 'She was <b>anxious</b> about not making her appointment on time.',
-    textExampleTranslate: 'Она беспокоилась о том, чтобы не договориться о встрече вовремя',
-    textMeaning: '<i>Anxious</i> means feeling worried or nervous.',
-    textMeaningTranslate: 'Тревожно означает чувствовать себя обеспокоенным или нервным',
-    transcription: '[ǽŋkʃəs]',
-    word: 'anxious',
-    wordTranslate: 'озабоченный',
-    wordsPerExampleSentence: 10,
-  }, {
-    audio: 'files/02_0621.mp3',
-    audioExample: 'files/02_0621_example.mp3',
-    audioMeaning: 'files/02_0621_meaning.mp3',
-    group: 1,
-    id: '5e9f5ee35eb9e72bc21af70c',
-    image: 'files/02_0621.jpg',
-    page: 1,
-    textExample: 'She was <b>anxious</b> about not making her appointment on time.',
-    textExampleTranslate: 'Она беспокоилась о том, чтобы не договориться о встрече вовремя',
-    textMeaning: '<i>Anxious</i> means feeling worried or nervous.',
-    textMeaningTranslate: 'Тревожно означает чувствовать себя обеспокоенным или нервным',
-    transcription: '[ǽŋkʃəs]',
-    word: 'anxious',
-    wordTranslate: 'озабоченный',
-    wordsPerExampleSentence: 10,
-  },
-];
+const constants = {
+  wordsPerPage: 10,
+  group: 0,
+};
 
-export { vocabularyPageLayout, navTabsLayout, tempWords };
+const filterQuery = {
+  learned: {
+    $and: [
+      { 'userWord.optional.vocabulary': { $ne: 'removed' } },
+      {
+        $or: [
+          { 'userWord.difficulty': 'easy' },
+          { 'userWord.difficulty': 'medium' },
+        ],
+      },
+    ],
+  },
+  difficult: {
+    $and: [
+      { 'userWord.optional.vocabulary': { $ne: 'removed' } },
+      { 'userWord.difficulty': 'hard' },
+    ],
+  },
+  deleted: { 'userWord.optional.vocabulary': 'removed' },
+};
+
+export {
+  pageLayout, navTabsLayout, constants, filterQuery,
+};
--- src/components/Vocabulary/Vocabulary.View.js
+++ src/components/Vocabulary/Vocabulary.View.js
@@ -1,48 +1,85 @@
+import STATUSES from 'services/requestHandler.Statuses';
 import BaseComponent from '../BaseComponent/BaseComponent';
-import { getPageLayout } from './Vocabulary.Layout';
-import { tempWords } from './Vocabulary.Data';
+import Words from '../../domain-models/Words/Words';
+import getPageLayout from './Vocabulary.Layout';
+import { pageLayout, constants, filterQuery } from './Vocabulary.Data';
 
 import './vocabulary.scss';
 
 class Vocabulary extends BaseComponent {
+  constructor(parent, tagName) {
+    super(parent, tagName);
+    this.wordsDomainModel = new Words(0);
+    this.categoryWordsAmount = 0;
+    this.todayWordsAmount = 0;
+  }
+
   createLayout() {
-    this.component.className = 'vocabulary';
+    this.component.className = pageLayout.container.className;
+    [
+      this.info,
+      this.nav,
+      [
+        this.wordsContainer,
+        this.words,
+      ],
+      this.pagination,
+    ] = getPageLayout({
+      allWordsNum: this.categoryWordsAmount,
+      todayWordsNum: this.todayWordsAmount,
+      words: this.words,
+    });
 
     this.component.append(
-      ...getPageLayout({ allWordsNum: 0, todayWordsNum: 0, words: tempWords }),
+      this.info,
+      this.nav,
+      this.wordsContainer,
+      this.pagination,
     );
-    // this.component.innerHTML = `
-    // <div class="vocabulary__info">
-    //   AMOUNT OF WORDS: 30 (ADDED TODAY: 0)
-    // </div>
-    // <nav class="vocabulary__nav">
-    //   <div class="vocabulary__tab vocabulary__tab--active">LEARNING WORDS</div>
-    //   <div class="vocabulary__tab">DIFFICULT WORDS</div>
-    //   <div class="vocabulary__tab">DELETED WORDS</div>
-    // </nav>
-    // <div class="vocabulary__list">
-    //   <div class="vocabulary__item">
-    //     <div class="vocabulary__word">anxious</div>
-    //     <button class="vocabulary__audio">play</button>
-    //     <div class="vocabulary__translation">озабоченный</div>
-    //     <div class="vocabulary__meaning">"<i>Anxious</i> means feeling worried or nervous."</div>
-    //     <div class="vocabulary__example">She was <b>anxious</b> about not making her appointment on time.</div>
-    //     <button class="vocabulary__delete">x</button>
-    //   </div>
-    //   <div class="vocabulary__item">
-    //     <div class="vocabulary__word">anxious</div>
-    //     <button class="vocabulary__audio">play</button>
-    //     <div class="vocabulary__translation">озабоченный</div>
-    //     <div class="vocabulary__meaning">"<i>Anxious</i> means feeling worried or nervous."</div>
-    //     <div class="vocabulary__example">She was <b>anxious</b> about not making her appointment on time.</div>
-    //     <button class="vocabulary__delete">x</button>
-    //   </div>
-    // </div>
-    // <div class="vocabulary__pagination">
-    //   <button class="vocabulary__prev"><<</button>
-    //   <button class="vocabulary__next">>></button>
-    // </div>
-    // `;
+  }
+
+  async prepareData() {
+    const filter = JSON.stringify(filterQuery.learned);
+
+    const wordsData = await this.wordsDomainModel.getAggregatedWords({
+      group: constants.group,
+      wordsPerPage: constants.wordsPerPage,
+      filter,
+    });
+    if (
+      STATUSES.isSuccess(wordsData.status)
+        && wordsData.data[0].totalCount[0]
+        && wordsData.data[0].totalCount[0].count
+    ) {
+      this.categoryWordsAmount = wordsData.data[0].totalCount[0].count;
+
+      this.words = wordsData.data[0].paginatedResults;
+      this.words.forEach((element) => {
+        // eslint-disable-next-line no-param-reassign
+        element.image = this.wordsDomainModel.getFileLink(element.image);
+      });
+    } else {
+      this.categoryWordsAmount = 0;
+      this.words = null;
+    }
+    console.log(wordsData);
+  }
+
+  playAudio(src) {
+    const audio = new Audio();
+    audio.preload = 'auto';
+    audio.src = this.wordsDomainModel.getFileLink(src.replace(/['"]+/g, ''));
+    audio.play();
+  }
+
+  addListeners() {
+    if (this.categoryWordsAmount) {
+      this.words.forEach((element) => element.addEventListener('click', (event) => {
+        if (event.target.dataset && event.target.dataset.audio) {
+          this.playAudio(event.target.dataset.audio);
+        }
+      }));
+    }
   }
 }
 
