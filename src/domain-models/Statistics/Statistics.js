984fbd6f9c421e9a760fe806ed5f725d46dc4b71
EvgeniyGuzenkov <jack199479@gmail.com>
2020-07-06 19:47:31 +0300

    fix: update data when a user has no data in backend

--- src/domain-models/Statistics/Statistics.js
+++ src/domain-models/Statistics/Statistics.js
@@ -1,3 +1,4 @@
+import STATUSES from 'services/requestHandler.Statuses';
 import endPoints from 'services/endPoints/endPoints.main';
 import BaseDomainModel from '../BaseDomainModel/BaseDomainModel';
 
@@ -11,16 +12,31 @@ class Statistics extends BaseDomainModel {
     return res;
   }
 
-  async updateStatistics(name, updateData) {
+  async updateStatistics(name, statistics) {
     const { data, status, statusText } = await this.getStatistics();
-    if (!data) {
+    if (
+      !data
+      && status !== STATUSES.NOT_FOUND
+    ) {
       return { status, statusText };
     }
 
-    const { optional } = data;
-    optional[name] = updateData;
+    let updateData;
+
+    if (status === STATUSES.NOT_FOUND) {
+      updateData = {
+        optional: {
+          [name]: statistics,
+        },
+      };
+    } else {
+      const { optional } = data;
+      optional[name] = statistics;
+      updateData = { optional };
+    }
+
     const res = await this.getDataOfAuthorizedUser(
-      update, this.userId, this.token, { optional },
+      update, this.userId, this.token, updateData,
     );
     return res;
   }
