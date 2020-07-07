4ac82d256b9be227648d49630ad00107ebd0aa07
EvgeniyGuzenkov <jack199479@gmail.com>
2020-07-07 14:14:16 +0300

    feat: add interval repetition method

--- src/domain-models/Words/Words.js
+++ src/domain-models/Words/Words.js
@@ -3,7 +3,9 @@ import endPoints from 'services/endPoints/endPoints.main';
 import STATUSES from 'services/requestHandler.Statuses';
 import BaseDomainModel from '../BaseDomainModel/BaseDomainModel';
 import { checkGroupWordsStatus, registrationWord } from './WordsHandler';
-import { VOCABULARY } from './Words.Constants';
+import {
+  VOCABULARY, OPTIONAL, REPEAT, DEFAULT_OPTIONS,
+} from './Words.Constants';
 
 const {
   getChunk,
@@ -17,6 +19,8 @@ const {
   getAggregatedWordData,
 } = endPoints.words;
 
+const pathToWordStatus = `userWord.optional.${OPTIONAL.REPEAT}.${REPEAT.STATUS}`;
+
 class Words extends BaseDomainModel {
   async getChunk(page, group) {
     const res = await this.getData(getChunk, page, group);
@@ -28,15 +32,15 @@ class Words extends BaseDomainModel {
     return res;
   }
 
-  async createUserWord(wordId, difficulty, vocabulary) {
-    const parameters = registrationWord({ optional: {} }, difficulty, vocabulary);
-    console.log(parameters)
+  async createUserWord(wordId, difficulty, vocabulary, options = DEFAULT_OPTIONS) {
+    const parameters = registrationWord({ optional: {} }, difficulty, vocabulary, options);
+    console.log(parameters);
     let res = await this.getDataOfAuthorizedUser(
       createUserWord, this.userId, this.token, wordId, parameters,
     );
 
     if (res.status === STATUSES.EXPECTATION_FAILED) {
-      res = await this.updateUserWord(wordId, difficulty, vocabulary);
+      res = await this.updateUserWord(wordId, difficulty, vocabulary, options);
     }
 
     return res;
@@ -56,7 +60,7 @@ class Words extends BaseDomainModel {
     return res;
   }
 
-  async updateUserWord(wordId, newDifficulty, newVocabulary) {
+  async updateUserWord(wordId, newDifficulty, newVocabulary, options = DEFAULT_OPTIONS) {
     const { data, status, statusText } = await this.getUserWordById(wordId);
     if (!data) {
       return { status, statusText };
@@ -64,7 +68,7 @@ class Words extends BaseDomainModel {
 
     const { difficulty, optional } = data;
     const parameters = registrationWord(
-      { difficulty, optional }, newDifficulty, newVocabulary,
+      { difficulty, optional }, newDifficulty, newVocabulary, options,
     );
 
     const res = await this.getDataOfAuthorizedUser(
@@ -91,8 +95,8 @@ class Words extends BaseDomainModel {
     const ALL_WORDS = 600;
     const filter = {
       $or: [
-        { 'userWord.optional.repeat.status': false },
-        { 'userWord.optional.repeat.status': true },
+        { [pathToWordStatus]: false },
+        { [pathToWordStatus]: true },
         { userWord: null }],
     };
 
@@ -114,9 +118,10 @@ class Words extends BaseDomainModel {
   }
 
   get repeatWords() {
+    const pathToVocabulary = `userWord.optional.${OPTIONAL.VOCABULARY}`;
     return this.groupWords.filter((word) => {
-      const status = get(word, 'userWord.optional.repeat.status');
-      const vocabulary = get(word, 'userWord.optional.vocabulary');
+      const status = get(word, pathToWordStatus);
+      const vocabulary = get(word, pathToVocabulary);
       return (
         !!status
         && (!vocabulary || vocabulary === VOCABULARY.RESTORED)
--- src/domain-models/Words/WordsHandler.js
+++ src/domain-models/Words/WordsHandler.js
@@ -1,12 +1,29 @@
 import get from 'lodash.get';
-import { DIFFICULTY, VOCABULARY } from './Words.Constants';
+import {
+  DIFFICULTY,
+  VOCABULARY,
+  OPTIONAL,
+  REPEAT,
+  REPEAT_INTERVAL,
+} from './Words.Constants';
 
-const TIMERS = {
-  [DIFFICULTY.EASY]: 7776000000,
-  [DIFFICULTY.MEDIUM]: 3888000000,
-  [DIFFICULTY.HARD]: 1123200000,
-  [DIFFICULTY.AGAIN]: 600000,
-};
+const MILLISECONDS_OF_DAY = 86400000;
+
+// ================== Interval repetition method ======================
+
+function getChangeForgettingSpeed(forgettingSpeed = 0.5, number) {
+  return forgettingSpeed ** number;
+}
+
+function getMemoryRating(rating = 0.9) {
+  return Math.log(rating);
+}
+
+function getDayOfRepeat(rating, forgettingSpeed, number) {
+  return (getMemoryRating(rating) / getChangeForgettingSpeed(forgettingSpeed, number)) ** 2;
+}
+
+// ====================================================================
 
 function checkWordStatus(repeatDate) {
   const date = new Date(repeatDate);
@@ -15,32 +32,61 @@ function checkWordStatus(repeatDate) {
 
 function checkGroupWordsStatus(group) {
   return group.map((word) => {
-    const repeat = get(word, 'userWord.optional.repeat');
+    const repeat = get(word, `userWord.optional.${[OPTIONAL.REPEAT]}`);
     if (!repeat) { return word; }
 
     const res = word;
-    res.userWord.optional.repeat.status = checkWordStatus(repeat.date);
+    res
+      .userWord
+      .optional[OPTIONAL.REPEAT][REPEAT.STATUS] = checkWordStatus(repeat[REPEAT.DATE]);
     return res;
   });
 }
 
-function registrationWord(data, difficulty, vocabulary) {
+function updateRepeatParameters(date) {
+  const updateParameters = {
+    [REPEAT.DATE]: Date.now() + date,
+    [REPEAT.STATUS]: false,
+  };
+  return updateParameters;
+}
+
+// ====================== registration word =============================
+
+function registrationWord(
+  data,
+  difficulty,
+  vocabulary,
+  options,
+) {
   const parameters = data;
 
-  parameters.optional.date = Date.now();
-  if (parameters.optional.amount === undefined) {
-    parameters.optional.amount = 0;
+  parameters.optional[OPTIONAL.DATE] = Date.now();
+  if (parameters.optional[OPTIONAL.AMOUNT] === undefined) {
+    parameters.optional[OPTIONAL.AMOUNT] = 0;
   }
-  parameters.optional.amount += 1;
+  parameters.optional[OPTIONAL.AMOUNT] += 1;
+
+  const forgettingSpeed = options[REPEAT_INTERVAL.FORGETTING_SPEED];
+  const rating = options[REPEAT_INTERVAL.RATING];
+  const maxAmount = options[REPEAT_INTERVAL.MAX_AMOUNT];
+  let amount = parameters.optional[OPTIONAL.AMOUNT];
+  amount = (amount > maxAmount ? maxAmount : amount);
+
+  const date = MILLISECONDS_OF_DAY * getDayOfRepeat(
+    rating, forgettingSpeed, amount,
+  );
+  parameters.optional[OPTIONAL.REPEAT] = updateRepeatParameters(date);
 
   if (DIFFICULTY[difficulty]) {
     parameters.difficulty = difficulty;
-    parameters.optional.repeat = {};
-    parameters.optional.repeat.date = Date.now() + TIMERS[difficulty];
-    parameters.optional.repeat.status = false;
+    parameters.optional[OPTIONAL.REPEAT] = updateRepeatParameters(
+      options[REPEAT_INTERVAL.TIMERS][DIFFICULTY[difficulty]],
+    );
   }
+
   if (VOCABULARY[vocabulary]) {
-    parameters.optional.vocabulary = vocabulary;
+    parameters.optional[OPTIONAL.VOCABULARY] = vocabulary;
   }
 
   return parameters;
