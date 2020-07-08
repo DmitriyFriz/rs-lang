d4f8b0185a4d393c2775d153b640698c4ff2bbcd
EvgeniyGuzenkov <jack199479@gmail.com>
2020-07-08 18:00:55 +0300

    feat: add interval repetition settings

--- src/components/Settings/Settings.View.js
+++ src/components/Settings/Settings.View.js
@@ -13,14 +13,22 @@ import getLayout from './Settings.Layout';
 // handler
 import { saveSettings, loadSettings } from './SettingsHandler';
 
+import { SETTINGS } from './Settings.Constants';
+
 class Settings extends BaseComponent {
   static get name() {
     return MAIN_ROUTES.SETTINGS;
   }
 
-  get settingsList() {
-    return this.component.querySelectorAll('[data-settings]');
-  }
+  // get mainSettingsList() {
+  //   const list = this.component.querySelectorAll(`[data-settings-${SETTINGS.MAIN}]`);
+  //   return { name: SETTINGS_MAIN, list };
+  // }
+
+  // get repetitionSettingsList() {
+  //   const list = this.component.querySelectorAll(`[data-settings-${SETTINGS.REPETITION}]`);
+  //   return { name: SETTINGS_REPETITION, list };
+  // }
 
   createLayout() {
     this.component.className = 'Settings';
@@ -34,7 +42,7 @@ class Settings extends BaseComponent {
   }
 
   addListeners() {
-    this.saveBtn.addEventListener('click', () => saveSettings(this.settingsList));
+    this.saveBtn.addEventListener('click', () => saveSettings(this.settingsData));
   }
 
   removeListeners() {
@@ -42,8 +50,39 @@ class Settings extends BaseComponent {
 
   async show() {
     await super.show();
-    await loadSettings(this.settingsList);
+    await this.handleSettings();
   }
+
+  getSettingsList(name) {
+    let list = this.component.querySelectorAll('[data-settings]');
+    list = [...list].filter((setting) => {
+      const [nameSettings] = setting.dataset.settings.split('.');
+      return nameSettings === name;
+    });
+    return { name, list };
+  }
+
+  async handleSettings(mode) {
+    const promises = Object.keys(SETTINGS).map(async (settingsName) => {
+      const settings = this.getSettingsList(SETTINGS[settingsName]);
+      if (mode === 'save') {
+        await saveSettings(settings);
+        return;
+      }
+      await loadSettings(settings);
+    });
+
+    await Promise.all(promises);
+  }
+
+  // async handleLoadSettings() {
+  //   const promises = Object.keys(SETTINGS).map(async (settingsName) => {
+  //     const settings = this.getSettingsList(SETTINGS[settingsName]);
+  //     await loadSettings(settings);
+  //   });
+
+  //   await Promise.all(promises);
+  // }
 }
 
 export default Settings;
--- src/components/Settings/SettingsHandler.js
+++ src/components/Settings/SettingsHandler.js
@@ -1,32 +1,33 @@
 import get from 'lodash.get';
 import SettingsDomain from '../../domain-models/Settings/Settings';
-import { DEFAULT_SETTINGS } from './Settings.Constants';
+import { DEFAULT_SETTINGS_LIST } from './Settings.Constants';
 
 const settingsDomain = new SettingsDomain();
 
-async function getSettings() {
+async function getSettings(name) {
   const { data } = await settingsDomain.getSettings();
-  const settings = get(data, 'optional.main');
-  return settings || DEFAULT_SETTINGS;
+  const settings = get(data, `optional.${name}`);
+  return settings || DEFAULT_SETTINGS_LIST[name];
 }
 
-async function saveSettings(settingsList) {
-  const settings = [...settingsList].reduce((accumulator, setting) => {
+async function saveSettings({ name, list }) {
+  const settings = [...list].reduce((accumulator, setting) => {
     const data = accumulator;
-    const settingName = setting.dataset.settings;
+    const settingName = setting.dataset.settings[name];
     data[settingName] = setting.type === 'checkbox'
       ? setting.checked : setting.value;
     return data;
-  }, {}); console.log(settings);
-  await settingsDomain.updateSettings('main', settings);
+  }, {});
+
+  await settingsDomain.updateSettings(name, settings);
 }
 
-async function loadSettings(settingsList) {
-  const settings = await getSettings(); console.log(settings);
+async function loadSettings({ name, list }) {
+  const settings = await getSettings(name);
 
-  [...settingsList].forEach((item) => {
+  [...list].forEach((item) => {
     const setting = item;
-    const settingName = setting.dataset.settings;
+    const [settingName] = setting.dataset.settings.split('.').reverse();
     const type = setting.type === 'checkbox' ? 'checked' : 'value';
     setting[type] = settings[settingName];
     return setting;
