38cd72933d503851d841a6e15ead0419e567d468
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2020-07-11 19:49:49 +0300

    feat: add main component & img.

---
+++ src/components/Games/Savannah/Savannah.Layout.js
@@ -0,0 +1,45 @@
+import logo from 'assets/mini-games-logo/savannahLogo.svg';
+import { GAMES_ROUTES } from 'router/Router.Constants';
+
+export default function getLayout() {
+  return `
+<div class="savannah-wrapper">
+<div class="game-message">
+      <img class="game-message__logo" src=${logo} alt="game logo">
+      <h1 class="game-message__title">Savannah</h1>
+    <fieldset class="fieldset">
+        <legend class="fieldset__legend">Select level</legend>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline1" value="0" name="chapter" checked>
+          <label class="fieldset__label" for="defaultInline1">1</label>
+        </div>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline2" value="1" name="chapter">
+          <label class="fieldset__label" for="defaultInline2">2</label>
+        </div>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline3" value="2" name="chapter">
+          <label class="fieldset__label" for="defaultInline3">3</label>
+        </div>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline4" value="3" name="chapter">
+          <label class="fieldset__label" for="defaultInline4">4</label>
+        </div>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline5" value="4" name="chapter">
+          <label class="fieldset__label" for="defaultInline5">5</label>
+        </div>
+        <div class="fieldset__control-inline">
+          <input class="fieldset__radio" type="radio" class="custom-control-input" id="defaultInline6" value="5" name="chapter">
+          <label class="fieldset__label" for="defaultInline6">6</label>
+        </div>
+      </fieldset>
+       <div class="game-message__button-container">
+        <button class="game-message__button" id="gameListBtn" data-destination=${GAMES_ROUTES.GAMES_LIST}>Back</button>
+        <button class="game-message__button" id="startGameBtn" data-destination=${GAMES_ROUTES.SAVANNAH_GAME}>Play</button>
+      </div>
+    </div>
+  </div>
+`;
+}
+
--- src/components/Games/Savannah/Savannah.js
+++ src/components/Games/Savannah/Savannah.js
@@ -1,49 +1,62 @@
 import BaseComponent from 'components/BaseComponent/BaseComponent';
-// import user from 'domainModels/User/User';
 import Words from 'domainModels/Words/Words';
 import './Savannah.scss';
+import { onRouteChangeEvent } from 'router/RouteHandler';
+import { ROUTERS } from 'router/Router.Constants';
+import getLayout from 'components/Games/Savannah/Savannah.Layout';
 
 export default class Savannah extends BaseComponent {
   constructor(parent, tagName) {
     super(parent, tagName);
     this.words = new Words();
     this.handlerCloseBtn = this.handlerCloseBtn.bind(this);
+    this.handlerStartGame = this.handlerStartGame.bind(this);
   }
 
   createLayout() {
-    this.component.classList.add('game-savanna');
-    const button = BaseComponent.createElement({
-      tag: 'button',
-      content: 'close',
-      id: 'closeBtn',
-      className: 'button',
-    });
-    this.component.append(button);
-    this.button = this.component.querySelector('#closeBtn');
+    this.body = document.body;
+    this.body.classList.add('game-savanna');
+    this.component.innerHTML = getLayout();
+    this.buttonBack = this.component.querySelector('#gameListBtn');
+    this.buttonGame = this.component.querySelector('#startGameBtn');
+    // const button = BaseComponent.createElement({
+    //   tag: 'button',
+    //   content: 'close',
+    //   id: 'closeBtn',
+    //   className: 'button',
+    //   destination: GAMES_ROUTES.GAMES_LIST,
+    // });
+    // this.component.append(button);
+    // this.button = this.component.querySelector('#closeBtn');
 
-    console.log(this.button);
-
-    this.words.getChunk(3, 0)
-      .then((result) => {
-        console.log(result);
-        const gameWords = result.data.map((el) => BaseComponent.createElement({
-          tag: 'h3',
-          content: `Words: ${el.word} - Translate: ${el.wordTranslate}`,
-          id: el.id,
-        }));
-        this.component.append(...gameWords);
-      });
+    // this.words.getChunk(3, 0)
+    //   .then((result) => {
+    //     console.log(result);
+    //     const gameWords = result.data.map((el) => BaseComponent.createElement({
+    //       tag: 'h3',
+    //       content: `Words: ${el.word} - Translate: ${el.wordTranslate}`,
+    //       id: el.id,
+    //     }));
+    //     this.component.append(...gameWords);
+    //   });
   }
 
   addListeners() {
-    this.button.addEventListener('click', this.handlerCloseBtn);
+    this.buttonBack.addEventListener('click', this.handlerCloseBtn);
+    this.buttonGame.addEventListener('click', this.handlerStartGame);
   }
 
   removeListeners() {
-    this.button.removeEventListener('click', this.handlerCloseBtn);
+    this.buttonBack.removeEventListener('click', this.handlerCloseBtn);
+    this.buttonGame.removeEventListener('click', this.handlerStartGame);
   }
 
-  handlerCloseBtn() {
-    console.log('close');
+  handlerCloseBtn(event) {
+    onRouteChangeEvent(event, ROUTERS.GAMES);
+    this.body.removeAttribute('class');
+  }
+
+  handlerStartGame(event) {
+    onRouteChangeEvent(event, ROUTERS.GAMES);
   }
 }
