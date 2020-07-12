e509c9e23ef00fbcd0cbe633eb0bf8ed871b9b8d
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-07-12 11:10:22 +0300

    fix: delete short statistic

--- src/components/Games/Sprint/Finish/SprintFinish.View.js
+++ src/components/Games/Sprint/Finish/SprintFinish.View.js
@@ -7,45 +7,25 @@ import finishGameAudio from 'assets/mini-games/audio/finish-game.mp3';
 // views
 import SprintStart from '../Start/SprintStart.View';
 
+// short statistic
+// import ShortStatistic from '../../ShortStatistic/ShortStatistic.View'
+
 // layout
 import getLayout from './SprintFinish.Layout';
-import getStatistic from './SprintFinish.Statistic';
 
 class SprintFinish extends SprintStart {
   constructor(parent, tagName) {
     super(parent, tagName);
 
     this.shortStatistic = JSON.parse(localStorage.getItem('sprint-shortStatistic'));
-    this.url = 'https://raw.githubusercontent.com/jack-guzya/rslang-data/master/';
-
-    this.handleClickStatistic = this.handleClickStatistic.bind(this);
   }
 
   createLayout() {
     this.component.innerHTML = getLayout();
     this.playAudio(finishGameAudio);
 
-    this.statisticList = getStatistic(this.shortStatistic);
-    this.component.firstElementChild.append(this.statisticList);
-  }
-
-  addListeners() {
-    super.addListeners();
-
-    this.statisticList.addEventListener('click', this.handleClickStatistic);
-  }
-
-  removeListeners() {
-    super.addListeners();
-
-    this.statisticList.removeEventListener('click', this.handleClickStatistic);
-  }
-
-  handleClickStatistic(event) {
-    if (event.target.dataset.audio) {
-      const src = this.url + event.target.dataset.audio;
-      this.playAudio(src);
-    }
+    // this.statistic = new ShortStatistic(this.component.firstElementChild, 'div', this.shortStatistic);
+    // this.statistic.show();
   }
 
   playAudio(src) {
