88d785f1d96673cbc9c3c0bee2f355b48b1b8880
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-07-15 23:53:24 +0300

    feat: add slider

--- src/components/Promo/Promo.View.js
+++ src/components/Promo/Promo.View.js
@@ -1,11 +1,37 @@
 // views
 import BaseComponent from 'components/BaseComponent/BaseComponent';
 
+// layout
+import getLayout from './Promo.Layout';
+
+// style
+import './Promo.scss';
+
+// Swiper
+import 'swiper/css/swiper.min.css';
+import Swiper from 'swiper';
+
 class Promo extends BaseComponent {
   createLayout() {
-    this.component.innerHTML = `
-      This is a promo page!
-    `;
+    this.component.innerHTML = getLayout();
+  }
+
+  async show() {
+    await super.show();
+
+    new Swiper('.promo-container', {
+      speed: 600,
+      parallax: true,
+      // grabCursor: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 }
 
